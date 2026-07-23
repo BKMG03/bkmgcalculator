06f51c35fc4ef9aad4797a91c90851ae36867e72
BKMG03 <iambablug@gmail.com>
2026-07-23 18:08:43 +0530

    Update ULIP Calculator Logic

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-function calculateULIP(){
+function calculateULIP() {
 
-const premium = Number(document.getElementById("premium").value);
-const ppt = Number(document.getElementById("ppt").value);
-const term = Number(document.getElementById("term").value);
-const rate = Number(document.getElementById("rate").value)/100;
+    let premium = Number(document.getElementById("premium").value);
+    let ppt = Number(document.getElementById("ppt").value);
+    let term = Number(document.getElementById("term").value);
+    let rate = Number(document.getElementById("rate").value) / 100;
 
-if(!premium || !ppt || !term){
-    alert("Please fill all fields");
-    return;
-}
-
-const totalPremium = premium * ppt;
-
-let fund = 0;
-
-for(let i=1;i<=term;i++){
-
-    if(i<=ppt){
-        fund += premium;
+    if (!premium || !ppt || !term) {
+        alert("Please fill all fields");
+        return;
     }
 
-    fund *= (1+rate);
-}
+    let totalPremium = premium * ppt;
+    let fund = 0;
 
-const gain = fund-totalPremium;
+    for (let i = 1; i <= term; i++) {
 
-document.getElementById("premiumResult").innerHTML =
-"₹"+totalPremium.toLocaleString("en-IN");
+        if (i <= ppt) {
+            fund += premium;
+        }
 
-document.getElementById("fundResult").innerHTML =
-"₹"+Math.round(fund).toLocaleString("en-IN");
+        fund = fund * (1 + rate);
+    }
 
-document.getElementById("gainResult").innerHTML =
-"₹"+Math.round(gain).toLocaleString("en-IN");
+    let gain = fund - totalPremium;
 
+    document.getElementById("premiumResult").innerHTML =
+        "₹" + Math.round(totalPremium).toLocaleString("en-IN");
+
+    document.getElementById("fundResult").innerHTML =
+        "₹" + Math.round(fund).toLocaleString("en-IN");
+
+    document.getElementById("gainResult").innerHTML =
+        "₹" + Math.round(gain).toLocaleString("en-IN");
 }
